a3d62f1e8c2154780c104b8bc3175be24f1ebd1a
Aleftina17 <teekupinmuotti@gmail.com>
2024-06-04 17:48:02 +0300

    feat: redirects

--- frontend/src/components/AuthForm.jsx
+++ frontend/src/components/AuthForm.jsx
@@ -7,11 +7,11 @@ const AuthForm = ({username, setUsername, password, setPassword, label, onSubmit
             <div className="auth_form__inputs">
                 <div className="auth_form__input">
                     <label htmlFor="username">Username:</label>
-                    <input type="text" id="username" value={username} className="input" onChange={(e) => setUsername(e.target.value)} />
+                    <input type="text" id="username" placeholder='Enter your username' value={username} className="input" onChange={(e) => setUsername(e.target.value)} />
                 </div>
                 <div className="auth_form__input">
                     <label htmlFor="password">Password:</label>
-                    <input type="password" id="password" value={password} className="input" onChange={(e) => setPassword(e.target.value)} />
+                    <input type="password" id="password" placeholder='Enter your password' value={password} className="input" onChange={(e) => setPassword(e.target.value)} />
                 </div>
             </div>
             <button className="btn btn_primary" type="submit">{label}</button>
