cc32a1c8b38f67560e6508bde68cebbd5497d728
Aleftina17 <teekupinmuotti@gmail.com>
2024-06-04 16:55:09 +0300

    fix: registration

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,37 +1,43 @@
 import React, { useState } from "react";
 import AuthForm from "./AuthForm";
-import axios from 'axios'
+import axios from "axios";
 import { useSnackbar } from "notistack";
 
 const Register = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
-    const { enqueueSnackbar } = useSnackbar()
+    const { enqueueSnackbar } = useSnackbar();
 
     const onSubmit = async (e) => {
-        e.preventDefault()
+        e.preventDefault();
         try {
-            await axios
-            .post("https://cookbook-mern.onrender.com/auth/register", {
+            await axios.post("https://cookbook-mern.onrender.com/auth/register", {
                 username,
-                password
-            })
-            enqueueSnackbar("Registration completed. Log into your account.", {variant: 'success'})
+                password,
+            });
+            enqueueSnackbar("Registration completed. Log into your account.", { variant: "success" });
         } catch (error) {
-            console.log(error)
+            if (error.response && error.response.status === 409) {
+                enqueueSnackbar("User already exists", { variant: "error" });
+            } else {
+                console.error(error);
+                enqueueSnackbar("An error occurred. Please try again.", { variant: "error" });
+            }
         }
-    }
+    };
 
-    return <AuthForm 
-    id='registerFrom'
-    username={username} 
-    setUsername={setUsername} 
-    password={password} 
-    setPassword={setPassword} 
-    description="Create an account by entering your username and password"
-    label= 'Register'
-    onSubmit={onSubmit}
-     />;
+    return (
+        <AuthForm
+            id="registerFrom"
+            username={username}
+            setUsername={setUsername}
+            password={password}
+            setPassword={setPassword}
+            description="Create an account by entering your username and password"
+            label="Register"
+            onSubmit={onSubmit}
+        />
+    );
 };
 
 export default Register;
