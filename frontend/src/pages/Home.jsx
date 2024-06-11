67d1e2e283f7ee0bfba282b32478d8beb5c33f21
Aleftina17 <teekupinmuotti@gmail.com>
2024-06-11 19:40:54 +0300

    feat: gsap

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,32 @@
-import React from "react";
-import { Link } from 'react-router-dom'
+import React, { useRef, useLayoutEffect } from "react";
+import { Link } from "react-router-dom";
 import { popularRecipes } from "../data/popularRecipes";
 import PopularCard from "../components/PopularCard";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import { useGSAP } from "@gsap/react";
 
 const Home = () => {
+    gsap.registerPlugin(useGSAP);
+    gsap.registerPlugin(ScrollTrigger);
+    // const container = useRef();
+
+    useLayoutEffect(() => {
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: ".home_hero .container",
+            },
+        });
+        tl.from(".home_hero .container", {
+            opacity: 0,
+            scale: 0.8,
+            duration: 2,
+        }).to(".home_hero .container", {
+            opacity: 1,
+            scale: 1,
+        });
+    }, []);
+
     return (
         <div className="home">
             <div className="home_hero">
@@ -11,8 +34,12 @@ const Home = () => {
                     <div className="home_hero__title">Best food for your taste</div>
                     <div className="home_hero__desc">Discover delectable cuisine and unforgettable moments in our welcoming, culinary haven.</div>
                     <div className="home_hero__btns">
-                        <Link to="/recipes" className="btn btn_primary"><span>Go to cookbook</span></Link>
-                        <Link to='/recipes/create' className="btn btn_secondary"><span>Add a recipe</span></Link>
+                        <Link to="/recipes" className="btn btn_primary">
+                            <span>Go to cookbook</span>
+                        </Link>
+                        <Link to="/recipes/create" className="btn btn_secondary">
+                            <span>Add a recipe</span>
+                        </Link>
                     </div>
                 </div>
             </div>
