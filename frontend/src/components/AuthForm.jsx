c73ccb9c806837af1b5e96417916e93a5faba868
Aleftina17 <teekupinmuotti@gmail.com>
2024-06-14 12:13:08 +0300

    feat: repeat pwd

--- frontend/src/components/AuthForm.jsx
+++ frontend/src/components/AuthForm.jsx
@@ -1,6 +1,18 @@
 import React from 'react'
 
-const AuthForm = ({username, setUsername, password, setPassword, label, onSubmit, id, description}) => {
+const AuthForm = ({
+    username,
+    setUsername,
+    password,
+    setPassword,
+    repeatPassword,
+    setRepeatPassword,
+    passwordError,
+    label,
+    onSubmit,
+    id,
+    description
+  }) => {
   return (
     <form className="auth_form" id={id} onSubmit={onSubmit}>
             <div className="auth_form__desc">{description}</div>
@@ -11,10 +23,17 @@ const AuthForm = ({username, setUsername, password, setPassword, label, onSubmit
                 </div>
                 <div className="auth_form__input">
                     <label htmlFor="password">Password:</label>
-                    <input type="password" id="password" placeholder='Enter your password' value={password} className="input" onChange={(e) => setPassword(e.target.value)} />
+                    <input type="password" id="password" placeholder='Enter your password' value={password} className="input" onChange={setPassword} />
                 </div>
+                {repeatPassword !== undefined && setRepeatPassword && (
+                    <div className={`auth_form__input ${passwordError ? 'error' : ''}`}>
+                    <label htmlFor="repeatPassword">Repeat Password:</label>
+                    <input type="password" id="repeatPassword" placeholder='Repeat your password' value={repeatPassword} className="input" onChange={setRepeatPassword} />
+                    {passwordError && <span className='error_message'>Passwords do not coincide. Try again.</span>}
+                </div>
+                )}
             </div>
-            <button className="btn btn_primary" type="submit">{label}</button>
+            <button className="btn btn_primary" type="submit"><span>{label}</span></button>
         </form>
   )
 }
--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -6,10 +6,18 @@ import { useSnackbar } from "notistack";
 const Register = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [repeatPassword, setRepeatPassword] = useState("");
+    const [passwordError, setPasswordError] = useState(false);
     const { enqueueSnackbar } = useSnackbar();
 
     const onSubmit = async (e) => {
         e.preventDefault();
+
+        if (password !== repeatPassword) {
+            setPasswordError(true);
+            return;
+        }
+
         try {
             await axios.post("https://cookbook-mern.onrender.com/auth/register", {
                 username,
@@ -18,7 +26,7 @@ const Register = () => {
             enqueueSnackbar("Registration completed. Log into your account.", { variant: "success" });
         } catch (error) {
             if (error.response && error.response.status === 409) {
-                enqueueSnackbar("User already exists", { variant: "error" });
+                enqueueSnackbar("User already exists.", { variant: "error" });
             } else {
                 console.error(error);
                 enqueueSnackbar("An error occurred. Please try again.", { variant: "error" });
@@ -26,13 +34,30 @@ const Register = () => {
         }
     };
 
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value)
+        if(passwordError){
+            setPasswordError(false)
+        }
+    }
+
+    const handlePasswordRepeatChange = (e) => {
+        setRepeatPassword(e.target.value)
+        if(passwordError){
+            setPasswordError(false)
+        }
+    }
+
     return (
         <AuthForm
             id="registerFrom"
             username={username}
             setUsername={setUsername}
             password={password}
-            setPassword={setPassword}
+            setPassword={handlePasswordChange}
+            repeatPassword={repeatPassword}
+            setRepeatPassword={handlePasswordRepeatChange}
+            passwordError={passwordError}
             description="Create an account by entering your username and password"
             label="Register"
             onSubmit={onSubmit}
